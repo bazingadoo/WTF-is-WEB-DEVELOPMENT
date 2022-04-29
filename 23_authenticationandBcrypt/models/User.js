d76cf2ab7c74bfbcf70d864089ce2f82f91b63ff
rawwr <akshayace0302@gmail.com>
2022-04-29 11:20:24 +0530

    refracting model methods

--- 23_authenticationandBcrypt/models/User.js
+++ 23_authenticationandBcrypt/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -13,6 +13,20 @@ const userSchema = new mongoose.Schema({
       "why on this earth do you want to leave your password empty",
     ],
   },
+});
+
+userSchema.statics.findAndValidate = async function (username, password) {
+  const foundUser = await this.findOne({ username });
+  const isValid = await bcrypt.compare(password, foundUser.password);
+  //if isValid is true, return foundUser, :(otherwise) false
+  return isValid ? foundUser : false;
+};
+
+//pre i.e before it saves the user information, the password gets hashed
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
